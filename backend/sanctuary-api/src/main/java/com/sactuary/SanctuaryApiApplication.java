de7ba9890a59d303e94ae0dfb55c455c98db6746
vhuthuphuthu <66826074+vhuthuphuthu@users.noreply.github.com>
2025-08-23 18:50:30 +0200

    first commit
    api

---
+++ backend/sanctuary-api/src/main/java/com/sactuary/SanctuaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.sactuary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanctuaryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanctuaryApiApplication.class, args);
+    }
+
+}
